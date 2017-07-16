e28eaef08eaaccf7384dc9ff8feb84a31898f274
Adam Arczynski <adam.arczynski@ocado.com>
2017-07-16 17:01:08 +0200

    remove unnecessary spring config

--- src/main/java/pl/arczynskiadam/demo/jhipster2/config/MVCConfig.java
+++ src/main/java/pl/arczynskiadam/demo/jhipster2/config/MVCConfig.java
@@ -1,8 +1,9 @@
 package pl.arczynskiadam.demo.jhipster2.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.*;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class MVCConfig extends WebMvcConfigurerAdapter {
@@ -10,11 +11,6 @@ public class MVCConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("forward:/views/index.html");
-    }
-    
-    @Override
-    public void configureViewResolvers(ViewResolverRegistry registry) {
-        registry.viewResolver(new InternalResourceViewResolver());
     }
 
     @Override
