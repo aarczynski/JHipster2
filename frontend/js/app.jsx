78572d09892c042ef4f92a6d3a080676307b4008
Adam Arczynski <adam.arczynski@ocado.com>
2017-08-16 16:09:30 +0200

    redux bootstrap

--- frontend/js/app.jsx
+++ frontend/js/app.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
 import Main from './components/Main';
 
+const reducer = (state = {}, action) => {
+    switch (action.type) {
+        case 'USERS_FETCH':
+            break;
+    }
+    return state;
+}
+
+const store = createStore(combineReducers({reducer}));
+
 ReactDOM.render(
-    <Main/>,
+    <Provider store={store}>
+        <Main/>
+    </Provider>,
     document.getElementById('react-app')
 );
