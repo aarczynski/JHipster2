9e8740191baf99c49a3fb1b38266bad1a04fddc4
Adam Arczynski <adam.arczynski@ocado.com>
2017-08-17 16:44:22 +0200

    demo commit - remove css extract

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,4 @@
 const path = require("path");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
 
@@ -35,34 +33,16 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    use: ['css-loader']
-                })
+                use: ['style-loader', 'css-loader']
             },
             {
                 test: /\.scss$/,
-                use: ExtractTextPlugin.extract({
-                    use: ['css-loader', 'sass-loader']
-                })
+                use: ['style-loader', 'css-loader', 'sass-loader']
             },
             {
                 test: /\.(woff2?|eot|ttf|svg)$/,
                 use: ['file-loader']
             }
         ]
-    },
-
-    plugins: [
-        new ExtractTextPlugin("bundle.css"),
-        new OptimizeCssAssetsPlugin({
-            assetNameRegExp: /\.css$/g,
-            cssProcessor: require('cssnano'),
-            cssProcessorOptions: {
-                discardComments: {
-                    removeAll: true
-                }
-            },
-            canPrint: true
-        })
-    ]
+    }
 }
